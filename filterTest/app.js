3bc5c2f4950822eb71c5041d307eb82d7a2dd641
Lucas <lucas.ecomp2012@gmail.com>
2016-11-09 17:36:54 -0300

    bug fix

--- filterTest/app.js
+++ filterTest/app.js
@@ -10,25 +10,10 @@
 //        key = matrix.type(temp).send(data);
 //      });
 
-var ledState = {};
-var arcDeg = 15;
 
-setInterval(function () {
-  arcDeg += 5;
-  if ( arcDeg > 60 ){
-    arcDeg = 10;
-  }
 
-  matrix.led(_.values(ledState)).render();
-}, 1000);
-
-matrix.init('temperature').then(function(data){
+matrix.init('temperature').then(function(data) {
   console.log('temperature >', data);
   matrix.type('temperature').send(data);
-  ledState.temp = {
-    arc: arcDeg,
-    color: 'blue',
-    start: 1,
-    spin: arcDeg
-  };
+  matrix.led('black').render();
 });
