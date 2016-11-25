819d7dce626f0c0a19e42a70fe2db871045a7c9f
Lucas <lucas.ecomp2012@gmail.com>
2016-11-25 18:34:44 -0300

    removing app configurations for clock example

--- ledTest/app.js
+++ ledTest/app.js
@@ -3,159 +3,8 @@
 //
 // have fun
 
-//first thing first
-
-//set colors
-/*
-setInterval(function() {
-  matrix.led('blue').render();
-}, 1000);
-
-//interleave two colors
-setInterval(function() {
-  matrix.led(['#bada55', '#e1ee7e']).render();
-}, 1000);
-
-setInterval(function() {
-  matrix.led(['blue', 'red']).render();
-}, 1000);
-
-
-//support opacity
-setInterval(function() {
-  matrix.led('rgba(255, 0, 100, 0.6)').render();
-}, 1000);
-
-
-setInterval(function() {
-  matrix.led('rgba(255, 255, 0, 1)').render();
-}, 1000);
-
-//generate shapes
-setInterval(function() {
-  matrix.led({
-    //degress of arc
-    arc: 90,
-
-    color: 'green',
-
-    //index to start drawing arc
-    start:12
-  }).render();
-}, 1000);
-
-
-setInterval(function() {
-  matrix.led({
-    //degress of arc
-    arc: 180,
-
-    color: 'blue',
-
-    //index to start drawing arc
-    start:12
-  }).render();
-}, 1000);
-
-
-//no color assumes off
-setInterval(function() {
-  matrix.led({ arc: 360 }).render();
-}, 1000);
-
-//no color assumes off
-setInterval(function() {
-  matrix.led({ color: 'red', arc: 200 }).render();
-}, 1000);
-
-
-//draw a point
-setInterval(function() {
-  matrix.led({
-    angle: 245,
-    color: 'white',
-    // blends interlight space if true, solid lights if false, default false
-    blend: true
-  }).render();
-}, 1000);
-
-
-//draw a point
-setInterval(function() {
-  matrix.led({
-    angle: 245,
-    color: 'green',
-    // blends interlight space if true, solid lights if false, default false
-    blend: false
-  }).render();
-}, 1000);
-
-
-//manipulate position
-//rotate the lights clockwise by a specified angle
-setInterval(function() {
-  matrix.led().rotate(90).render();
-}, 1000);
-
-
-setInterval(function() {
-  matrix.led({ color: '#d3d3d3'}).rotate(180).render();
-}, 1000);
-
-
-setInterval(function() {
-  matrix.led({ color: '#4b0082'}).rotate(270).render();
-}, 1000);
-
-
-setInterval(function() {
-  matrix.led({ color: 'blue'}).rotate(360).render();
-}, 1000);
-
-
-//shape objects
-//make a smiley face
-setInterval(function() {
-  matrix.led([
-    {
-      angle: 45,
-      color: 'yellow'
-    },
-    {
-      angle: 135,
-      color: 'yellow'
-    },
-    {
-      arc: 90,
-      color: 'yellow',
-      start: 225
-    }
-  ]).render();
-}, 1000);
-
-*/
-//direct pixel manipulation
-//(0) -> 0, (1) -> 0, (2) -> 0, (3) -> 0, (4) -> yellow, (5) -> 0, (6) -> 0
-//(7) -> 0, (8) -> 0, (9) -> 0, (10) -> 0, (11) -> 0, (12) -> 0, (13) -> yellow,
-//(14) -> 0, (15) -> 0, (16) -> 0, (17) -> 0, (18) -> 0, (19) -> 0, (20) -> 0, (21) -> 0,
-//(22) -> yellow,  (23) -> yellow, (24) -> yellow, (25) -> yellow, (26) -> yellow, (27) -> yellow,
-//(28) -> yellow, (29) -> yellow, (30) -> yellow, (31) -> yellow, (32) -> yellow, (33) -> yellow,
-//(34) -> yellow, (35) -> yellow
-setInterval(function() {
-
-  matrix.led(['purple', 0, 0, 0, 'blue', 0,
-    0, 0, 0, 0, 0, 0, 0, 'blue', 0, 0,
-    0, 0, 0, 0, 0, 0, 'blue', 'blue',
-    'blue', 'blue', 'green', 'white',
-    'red', 'red', 'blue', 'blue', 'blue',
-    'blue', 'blue', 'blue']).render();
-
-}, 1000);
-
-/*
 //example clock
-
-setInterval(function(){
+var clock = function(){
   var time = new Date();
   var h = time.getHours();
   var m = time.getMinutes();
@@ -180,11 +29,46 @@ setInterval(function(){
     color: 'yellow',
     blend: true
   };
-  matrix.type('clock').send({
-    'hourAngle': hourLed.arc,
-    'minuteAngle': minuteLed.angle,
-    'secondAngle': secondLed.angle
-  });
 
   matrix.led([hourLed, minuteLed]).render();
-}, 1000);*/
+};
+
+var examples = [
+  matrix.led('blue').render,
+  matrix.led(['#bada55', '#e1ee7e']).render, //interleave two colors
+  matrix.led(['blue', 'red']).render, //interleave two colors
+  matrix.led('rgba(255, 0, 100, 0.6)').render, //support opacity
+  matrix.led('rgba(255, 255, 0, 1)').render, //support opacity
+  matrix.led({ arc: 90, color: 'green', start:12 }).render, //generate shapes
+  matrix.led({ arc: 180, color: 'blue', start:12 }).render, //generate shapes
+  matrix.led({ arc: 360 }).render, //no color assumes off
+  matrix.led({ color: 'red', arc: 200 }).render, //red arc
+  matrix.led({ angle: 245, color: 'white', blend: true }).render, //draw a point
+  matrix.led({ angle: 245, color: 'green', blend: false }).render, //draw a point
+  matrix.led().rotate(90).render, //manipulate position//rotate the lights clockwise by a specified angle
+  matrix.led({ color: '#d3d3d3'}).rotate(180).render,//manipulate position//rotate the lights clockwise by a specified angle
+  matrix.led({ color: '#4b0082'}).rotate(270).render,//manipulate position//rotate the lights clockwise by a specified angle
+  matrix.led({ color: '#blue'}).rotate(360).render,//manipulate position//rotate the lights clockwise by a specified angle
+  matrix.led([ {
+                  angle: 45,
+                  color: 'yellow'
+                },
+                {
+                  angle: 135,
+                  color: 'yellow'
+                },
+                {
+                  arc: 90,
+                  color: 'yellow',
+                  start: 225
+                }
+              ]).render, //shape objects, make a smiley face
+  matrix.led(['purple', 0, 0, 0, 'blue', 0, 0, 0, 0, 0, 0, 0, 0, 'blue', 0, 0,
+                0, 0, 0, 0, 0, 0, 'blue', 'blue', 'blue', 'blue', 'green', 'white',
+                'red', 'red', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue']).render, //direct pixel manipulation
+  clock
+];
+var i = 0;
+setInterval(function(){
+  examples[(i++)%examples.length]();
+}, 3000);
