56f670042909b1c43d986569a2adbd68fd8a6965
Sean Canton <eighteyes@gmail.com>
2016-11-25 15:41:08 -0800

    Add mics

--- micTest/app.js
+++ micTest/app.js
@@ -1,7 +1,83 @@
-// app code goes here
-// matrix.init()....
-//
-// have fun
-matrix.init('mic').then(function(data){
-  console.log('><><><><><><>< MICRO PHONE ><><><><><><><><', data);
+matrix.init('mic', {number: 0}).then(function(data){
+  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  (data.value < 200 ) ? 'orange' : 'red';
+
+  matrix.led({
+    arc: 0,
+    color: { r: data.value }
+  }).render();
 });
+
+matrix.init('mic', {number: 1}).then(function(data){
+  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  (data.value < 200 ) ? 'orange' : 'red';
+
+  matrix.led({
+    arc: 30,
+    color: { r: data.value }
+  }).render();
+});
+
+matrix.init('mic', {number: 2}).then(function(data){
+  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  (data.value < 200 ) ? 'orange' : 'red';
+
+  matrix.led({
+    arc: 60,
+    color: { r: data.value }
+  }).render();
+});
+
+matrix.init('mic', {number: 3}).then(function(data){
+  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  (data.value < 200 ) ? 'orange' : 'red';
+
+  matrix.led({
+    arc: 90,
+    color: { r: data.value }
+  }).render();
+});
+
+matrix.init('mic', {number: 4}).then(function(data){
+  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  (data.value < 200 ) ? 'orange' : 'red';
+
+  matrix.led({
+    arc: 120,
+    color: { r: data.value }
+  }).render();
+});
+
+matrix.init('mic', {number: 5}).then(function(data){
+  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  (data.value < 200 ) ? 'orange' : 'red';
+
+  matrix.led({
+    arc: 150,
+    color: { r: data.value }
+  }).render();
+});
+
+matrix.init('mic', {number: 6}).then(function(data){
+  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  (data.value < 200 ) ? 'orange' : 'red';
+
+  matrix.led({
+    arc: 180,
+    color: { r: data.value }
+  }).render();
+});
+
+matrix.init('mic', {number: 7}).then(function(data){
+  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  (data.value < 200 ) ? 'orange' : 'red';
+
+  matrix.led({
+    arc: 210,
+    color: { r: data.value }
+  }).render();
+});
+
+
+//  var c = ( data.value < 50 )? 'green' : ( data.value < 125 ) ? 'yellow' :
+  // (data.value < 200 ) ? 'orange' : 'red';
