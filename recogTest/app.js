46074ec6107212a307ef6b2352107633cd0b705b
Sean Canton <eighteyes@gmail.com>
2017-02-24 12:01:30 -0800

    update recog

--- recogTest/app.js
+++ recogTest/app.js
@@ -1,21 +1,20 @@
 matrix.on('train', function(d) {
+  var trained = false;
+  console.log('training started>>>>>', d);
   matrix.init('recognition', { mode: 'train', tag: 'test' }).then(function(d) {
-    if (d.hasOwnProperty('count')) {
+    if (!trained && d.hasOwnProperty('count')) {
       // means it's partially done
       matrix.led({
         arc: Math.round(360 * (d.count / d.target)),
         color: 'blue'
       }).render();
+    } else {
+      trained = true;
+      matrix.led('blue');
+      console.log('trained!', d);
     }
-    console.log('trained!', d);
   });
-  console.log('training started>>>>>', d);
 });
-
-//
-// matrix.init('recognition', {tag: 'test'}).then(function (d) {
-//   console.log('RECOGNIZED!!>!!>!>>!>!>!>', d);
-// })
 
 matrix.on('recog', function(d) {
   matrix.init('recognition', { tag: 'test' }).then(function(d) {
